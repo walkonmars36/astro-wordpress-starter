cbfea7036ee16bcf9e13f8caaa2b280d7a642222
Mark Lawson <mark@walkonmars.co.uk>
2023-03-27 21:41:04 +0100

    hooked up to WP

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ export async function navQuery(){
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify({
             query: `{
-                menus(where: {location: PRIMARY}) {
+                menus(where: {location: MENU_1}) {
                   nodes {
                     name
                     menuItems {
